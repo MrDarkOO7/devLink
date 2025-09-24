47ebf294a6c1c27d4d7af82d9a69b14f741866b6
MrDarkOO7 <m.waleedapsacian@gmail.com>
2025-09-24 18:38:42 +0500

    Refactor user model and enhance API endpoints for user management

--- src/app.js
+++ src/app.js
@@ -3,23 +3,83 @@ const connectDB = require("./config/database");
 const UserModel = require("./models/user");
 
 const app = express();
+app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  const userObj = {
-    firstName: "Waleeddd",
-    lastName: "Idre",
-    emailId: "m.waled@gmail.co",
-    password: "Testing@123",
-    age: 22,
-    gender: "Male",
-  };
+  console.log("Request body:", req.body);
+  const userData = req.body;
 
-  const user = new UserModel(userObj);
+  const user = new UserModel(userData);
   try {
     await user.save();
     res.send("User signed up successfully");
   } catch (err) {
     res.status(400).send("Error signing up user: ", err.message);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const email = req.body?.emailId;
+  if (!email) {
+    return res.status(400).send("Email is required");
+  }
+  console.log("Fetching user with email:", email);
+  try {
+    const user = await UserModel.findOne({ emailId: email });
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    res.json(user);
+  } catch (err) {
+    return res.status(500).send("Error fetching user: ", err.message);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await UserModel.find({});
+    if (!users || users.length === 0) {
+      return res.status(404).send("No users found");
+    }
+    res.json(users);
+  } catch (err) {
+    return res.status(500).send("Error fetching user: ", err.message);
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const data = req.body;
+  if (!data) {
+    return res.status(400).send("user data is required");
+  }
+  console.log("Updating user with userId:", data?.userId);
+  try {
+    const result = await UserModel.findByIdAndUpdate(data?.userId, data);
+    console.log(result);
+    if (!result) {
+      return res.status(404).send("User not found");
+    }
+    res.send("User updated successfully");
+  } catch (err) {
+    return res.status(500).send("Error updating user: ", err.message);
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body?.userId;
+  if (!userId) {
+    return res.status(400).send("userId is required");
+  }
+  console.log("Deleting user with userId:", userId);
+  try {
+    const result = await UserModel.findByIdAndDelete(userId);
+    console.log(result);
+    if (result.deletedCount === 0) {
+      return res.status(404).send("User not found");
+    }
+    res.send("User deleted successfully");
+  } catch (err) {
+    return res.status(500).send("Error deleting user: ", err.message);
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -2,15 +2,33 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  firstName: { type: String },
-  lastName: { type: String },
-  emailId: { type: String },
-  password: { type: String },
-  age: { type: Number },
-  gender: { type: String },
-  createdAt: { type: Date, default: Date.now },
-});
+const userSchema = new Schema(
+  {
+    firstName: { type: String, required: true, minLength: 4, maxLength: 30 },
+    lastName: { type: String },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: { type: String, required: true },
+    age: { type: Number, min: 18 },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "other"].includes(value)) {
+          throw new Error("Invalid gender");
+        }
+      },
+    },
+    photoUrl: { type: String },
+    skills: { type: [String], default: [] },
+    bio: { type: String, maxLength: 250 },
+  },
+  { timestamps: true }
+);
 
 const UserModel = mongoose.model("User", userSchema);
 
